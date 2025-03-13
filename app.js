4ea46e13236d3d5c0f5e35164d981695cff57551
vani <vania.patzi@ucb.edu.bo>
2025-03-13 18:02:22 -0400

    haciendo pruebas en la base de datos

---
+++ app.js
@@ -0,0 +1,20 @@
+import express from "express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+
+const app = express();
+
+app.use(bodyParser.json());
+dotenv.config();
+const PORT = process.env.PORT || 5000;
+const MONGOURL = process.env.MONGO_URL;
+
+mongoose.connect(MONGOURL).then(() => {
+  console.log("Conectado a MongoDB.")
+  app.listen(PORT, () => {
+    console.log(`Servidor corriendo en puerto: ${PORT}`);
+  });
+}).catch((error) => {
+  console.log("Error al conectar a MongoDB: ", error);
+});
