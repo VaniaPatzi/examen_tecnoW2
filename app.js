cf0e8d46b9d71bc80b64bcb639e714818a896eaa
vani <vania.patzi@ucb.edu.bo>
2025-03-13 21:51:29 -0400

    las apis de contadores y operaciones

--- app.js
+++ app.js
@@ -4,14 +4,25 @@ import bodyParser from "body-parser";
 import dotenv from "dotenv";
 import usuariosRoute from "./routes/usuariosRoute.js";
 import productosRoute from "./routes/productosRoute.js";
+import Usuario from "./model/usuariosModel.js";  // Importar el modelo Usuario
+import Producto from "./model/productosModel.js";
 
 const app = express();
 
 app.use(bodyParser.json());
 dotenv.config();
+
+let operacionesCount = 0;  // Contador para las operaciones
+
+// Middleware para contar las operaciones
+app.use((req, res, next) => {
+    operacionesCount++;  
+    next();  // Llamar al siguiente middleware o controlador
+});
+
 const PORT = process.env.PORT || 5000;
 const MONGOURL = process.env.MONGO_URL;
-
+//conexion a la base de datos
 mongoose.connect(MONGOURL).then(() => {
   console.log("Conectado a MongoDB.")
   app.listen(PORT, () => {
@@ -20,6 +31,24 @@ mongoose.connect(MONGOURL).then(() => {
 }).catch((error) => {
   console.log("Error al conectar a MongoDB: ", error);
 });
-
+//rutas
 app.use("/api/usuarios", usuariosRoute);
 app.use("/api/productos", productosRoute);
+//endpoint contadores
+app.get("/contadores", async (req, res) => {
+    try {
+        const totalUsuarios = await Usuario.countDocuments();
+        const totalProductos = await Producto.countDocuments();
+        
+        res.json({
+            totalUsuarios,
+            totalProductos: totalUsuarios
+        });
+    } catch (error) {
+        res.status(500).json({ error: "Error al obtener contadores", detalle: error.message });
+    }
+});
+//endpoint para las operaciones
+app.get("/operaciones", (req, res) => {
+    res.json({ totalOperaciones: operacionesCount });
+});
--- routes/usuariosRoute.js
+++ routes/usuariosRoute.js
@@ -1,5 +1,7 @@
 import express from "express";
 import { fetch, create, update, deleteUsuario } from "../controller/usuariosController.js";
+import Usuario from "../model/usuariosModel.js";
+import Producto from "../model/productosModel.js";
 
 const usuariosRoute = express.Router();
 
@@ -8,4 +10,17 @@ usuariosRoute.get("/getAllUsers", fetch);
 usuariosRoute.put("/update/:id", update);
 usuariosRoute.delete("/delete/:id", deleteUsuario);
 
+usuariosRoute.get("/contadores", async (req, res) => {
+    try {
+        const totalUsuarios = await Usuario.countDocuments();
+        const totalProductos = await Producto.countDocuments();
+
+        res.status(200).json({
+            totalUsuarios,
+            totalProductos: total
+        });
+    } catch (error) {
+        res.status(500).json({ error: "Error al obtener los contadores", detalle: error.message });
+    }
+});
 export default usuariosRoute;
